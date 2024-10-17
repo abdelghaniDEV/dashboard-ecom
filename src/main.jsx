126d97f88c391169285698131e2a0eb0f53f4b18
abdelghaniDEV <abdelghaniberhouch2001@gmail.com>
2024-10-17 02:50:41 +0200

    fexxed

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { store } from './Redux/store.jsx'
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
-   <BrowserRouter >
+   <BrowserRouter basename='/dashboard-ecome-vite/' >
     <Provider store={store}>
     <App />
     </Provider>
